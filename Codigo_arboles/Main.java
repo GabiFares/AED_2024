759847069a88cf73f0965d860f4021b2ba70f517
Gabi <computadorafares18@gmail.com>
2024-05-15 18:45:27 -0300

    codigo_Arboles

---
+++ Codigo_arboles/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        TArbolBB arbol = new TArbolBB();
+
+      // cargar los productos desde el archivo "altas.txt"
+      
+
+    }
+}
+
