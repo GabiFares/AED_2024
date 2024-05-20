8f2a91409b005e3e8ff6aa731e0ad3a17b44a440
Gabi <computadorafares18@gmail.com>
2024-05-20 18:40:10 -0300

    20/05

---
+++ UT4_TA9/TA9 - Codigo/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        TArbolBB arbol = new TArbolBB();
+
+      // cargar los productos desde el archivo "altasprueba.txt"
+      // listar los productos ordenados por codigo, junto con su cantidad existente
+      //emitir el valor del stock
+      // simular las ventas a partir del archivo "ventaspruebas.txt"
+      // simular la eliminación de productos a partir del archivo "elimPrueba.txt"
+      // listar los productos ordenados por codigo, junto con su cantidad existente
+
+    }
+}
+
